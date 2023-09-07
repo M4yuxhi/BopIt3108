39adbe2408c6faabf857ec96be49e7f596d86a5e
Maybomi Tobar <maybomi.unu@gmail.com>
2023-09-07 11:05:21 -0300

    Splash Screen Added part 2

---
+++ app/src/main/java/com/maybomiTobar/bopit_31_08/MainMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.maybomiTobar.bopit_31_08
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
