1c502c7628a43c57f64a67c21f320ce0dd70e84a
Maybomi Tobar <maybomi.unu@gmail.com>
2023-09-14 10:34:25 -0300

    Time delay of SplashActivity

--- app/src/main/java/com/maybomiTobar/bopit_31_08/MainActivity.kt
+++ app/src/main/java/com/maybomiTobar/bopit_31_08/MainActivity.kt
@@ -2,19 +2,25 @@ package com.maybomiTobar.bopit_31_08
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
 
 class MainActivity : AppCompatActivity()
 {
-    //Variable Global - Global Variable
-    private var generalCount = 0;
-
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val delayMillis = 3000L //Segundos de espera.
+        var handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            startActivity(intent);
+            finish()
+        }, delayMillis)
     }
 
 
