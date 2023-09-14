86732bf03ec95cc4f49bbad9945ec519dab024ae
Maybomi Tobar <maybomi.unu@gmail.com>
2023-09-14 09:23:09 -0300

    Correct define of string attributes and about button added

--- app/src/main/java/com/maybomiTobar/bopit_31_08/MainActivity.kt
+++ app/src/main/java/com/maybomiTobar/bopit_31_08/MainActivity.kt
@@ -15,26 +15,7 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //Variable Local - Local Variable
-        val localCount = 0
-
-        //Find Views
-        /*
-        val buttonIncrement = findViewById<Button>(R.id.buttonIncrement)
-        val buttonNavigate = findViewById<Button>(R.id.buttonNavigate)
-
-        //Button click listeners
-        buttonIncrement.setOnClickListener
-        {
-            // Increment the global counter
-            generalCount++
-            // Update the text on a TextView (not shown in this example)
-        }
-
-        buttonNavigate.setOnClickListener
-        {
-
-        }*/
     }
+
+
 }
