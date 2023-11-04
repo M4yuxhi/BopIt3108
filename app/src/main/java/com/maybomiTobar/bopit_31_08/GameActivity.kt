df36945690e82395365b017db0b887d6d055acad
M4yuxhi <91566436+M4yuxhi@users.noreply.github.com>
2023-11-03 23:26:41 -0300

    Update 3 Complete!!

--- app/src/main/java/com/maybomiTobar/bopit_31_08/GameActivity.kt
+++ app/src/main/java/com/maybomiTobar/bopit_31_08/GameActivity.kt
@@ -3,31 +3,47 @@ package com.maybomiTobar.bopit_31_08
 import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.media.MediaPlayer
 import android.media.PlaybackParams
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import kotlin.math.roundToInt
+import kotlin.random.Random
+import kotlin.math.abs
 
-class GameActivity : AppCompatActivity()
+class GameActivity : AppCompatActivity(), SensorEventListener
 {
     private lateinit var backgroundMediaPlayer : MediaPlayer
     private lateinit var fxMediaPlayer : MediaPlayer
     private lateinit var playbackParams: PlaybackParams
-    private var bgMusicVolume : Float = 0.3f
+    private var bgMusicVolume : Float = 0.2f
     private lateinit var volumeValueTV : TextView
 
     private lateinit var gestureDetector : GestureDetector
 
     private lateinit var sensorManager : SensorManager
-    private lateinit var accelerometerSensor : Sensor
-    private lateinit var sensorActiveList : ArrayList<Boolean>
+    private var accelerometerSensor : Sensor? = null
+    private var prevValueAcceleration = 0.0f
+    private var currentInstruction = " "
+    private lateinit var instructionTV: TextView
+    private lateinit var instructionTimeTV : TextView
+    private lateinit var possibleIntructions : Array<String>
+    private var instructionTimer : CountDownTimer? = null
+    private val instructionTime = 5000
+
+    private lateinit var scoreTV : TextView
+    private var score = 0
+    private var asignedScore = 100
+
+    private var actionWasPerformed = false
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -38,6 +54,11 @@ class GameActivity : AppCompatActivity()
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        instructionTV = findViewById(R.id.instructionTVG)
+        instructionTimeTV = findViewById(R.id.instructionTimeTVG)
+        scoreTV = findViewById(R.id.scoreTVG2)
+        possibleIntructions = arrayOf("Toca Bop It", "Desliza", "Agita")
 
         initializeBGMediaPlayer()
 
@@ -50,38 +71,45 @@ class GameActivity : AppCompatActivity()
         super.onPause()
         backgroundMediaPlayer.pause()
         fxMediaPlayer.stop()
+
+        sensorManager.unregisterListener(this)
     }
 
     override fun onResume()
     {
         super.onResume()
         backgroundMediaPlayer.start()
+
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
-/*
-    override fun onSensorChanged(event : SensorEvent)
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int)
+    {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent)
     {
         if(event.sensor.type == Sensor.TYPE_ACCELEROMETER)
         {
-            var dx = Math.pow(event.values[0].toDouble(), 2.0)
-            var dy = Math.pow(event.values[1].toDouble(), 2.0)
-            var dz = Math.pow(event.values[2].toDouble(), 2.0)
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
 
-            var acceleration = Math.sqrt(dx + dy + dz).toFloat()
+            val diff = abs(x + y + z - prevValueAcceleration)
+            val scaleFactor = Math.pow(10.0, 10.0).toFloat()
+            val acceleration = (diff / event.timestamp) * scaleFactor
 
-            val threshold = 10.0f
+            prevValueAcceleration = x + y + z
 
-            if(acceleration > threshold)
-            {
-                //Revisa si la aceleración es valida para ser una agitación.
+            val threshold = 0.01f
 
-            }
-            else
-            {
+            if(acceleration > threshold && currentInstruction == possibleIntructions[2])
+                correctPerformance()
 
-            }
         }
     }
-    */
+
 
     override fun onTouchEvent(event: MotionEvent): Boolean
     {
@@ -89,20 +117,63 @@ class GameActivity : AppCompatActivity()
         return super.onTouchEvent(event)
     }
 
-    private fun changeSoundSpeed(option : Int)
+    private fun bopItLogic()
     {
-        if(option == 1)
+        scoreTV.text = score.toString()
+
+        val random = Random
+        val randomNumber = random.nextInt(1, 4)
+
+        currentInstruction = possibleIntructions[randomNumber - 1]
+        instructionTV.text = currentInstruction
+
+        instructionTimer?.cancel()
+
+        instructionTimer = object : CountDownTimer(instructionTime.toLong(), 1000)
         {
-            backgroundMediaPlayer.playbackParams.setSpeed(playbackParams.speed + 0.1f)
-            return
+
+            override fun onTick(millisUntilFinished : Long)
+            {
+                instructionTimeTV.text = (1 + millisUntilFinished / 1000).toString()
+            }
+
+            override fun onFinish()
+            {
+                if(!actionWasPerformed)
+                {
+                    setMusicOnFXMP(R.raw.lose_sound)
+                }
+
+                fxMediaPlayer.start()
+                bopItLogic()
+            }
+
         }
 
-        backgroundMediaPlayer.playbackParams.setSpeed(playbackParams.speed - 0.1f)
+        instructionTimer?.start()
+        actionWasPerformed = false
+    }
+
+    private fun changeSoundSpeed(scalar : Float)
+    {
+        backgroundMediaPlayer.playbackParams.setSpeed(playbackParams.speed * scalar)
+    }
+
+    private fun correctPerformance()
+    {
+        //showToast(R.string.correctActionMessage.toString())
+        actionWasPerformed = true
+        score += asignedScore
+        setMusicOnFXMP(R.raw.win_sound)
+        bopItLogic()
     }
 
     private fun handlers()
     {
+        val handlerLogic = Handler()
+        val milliseconds = 500
 
+        handlerLogic.postDelayed({ bopItLogic() }, milliseconds.toLong())
     }
 
     private fun initializeBGMediaPlayer()
@@ -112,9 +183,6 @@ class GameActivity : AppCompatActivity()
         backgroundMediaPlayer.setVolume(bgMusicVolume, bgMusicVolume)
 
         playbackParams = backgroundMediaPlayer.playbackParams
-
-        volumeValueTV = findViewById(R.id.textViewVolumeValueG)
-        volumeValueTV.text = (bgMusicVolume * 10).roundToInt().toString()
     }
 
     private fun restartSoundSpeed()
@@ -130,49 +198,47 @@ class GameActivity : AppCompatActivity()
 
     private fun setOnClickListeners()
     {
-        val buttonLessVolume : Button = findViewById(R.id.buttonLessVolume)
-        val buttonMoreVolume : Button = findViewById(R.id.buttonMoreVolume)
+        val buttonBopIt : Button = findViewById(R.id.bopItButtonG)
 
-        buttonLessVolume.setOnClickListener()
+        buttonBopIt.setOnClickListener()
         {
-            if(bgMusicVolume >= 0.1f)
+            if(currentInstruction == possibleIntructions[0])
             {
-                bgMusicVolume -= 0.1f
-                backgroundMediaPlayer.setVolume(bgMusicVolume, bgMusicVolume)
-                volumeValueTV.text = (bgMusicVolume * 10).roundToInt().toString()
-            }
-        }
-
-        buttonMoreVolume.setOnClickListener()
-        {
-            if(bgMusicVolume <= 0.9f)
-            {
-                bgMusicVolume += 0.1f
-                backgroundMediaPlayer.setVolume(bgMusicVolume, bgMusicVolume)
-                volumeValueTV.text = (bgMusicVolume * 10).roundToInt().toString()
+                correctPerformance()
             }
         }
     }
 
     inner class GestureListener : GestureDetector.SimpleOnGestureListener()
     {
+
+        /*
         override fun onDown(e : MotionEvent) : Boolean
         {
             showToast("onDown")
-            return true
-        }
 
+            if(currentInstruction == possibleIntructions[0])
+            {
+                correctPerformance()
+            }
+
+            return true
+        }*/
+
+        /*
         override fun onSingleTapUp(e : MotionEvent) : Boolean
         {
             showToast("onSingleTapUp")
             return true
-        }
+        }*/
 
+        /*
         override fun onLongPress(e : MotionEvent)
         {
             showToast("onLongPress")
         }
-
+        */
+        /*
         override fun onFling(
             e1: MotionEvent, e2: MotionEvent,
             velocityX : Float, velocityY: Float
@@ -181,6 +247,7 @@ class GameActivity : AppCompatActivity()
             showToast("onFling")
             return true
         }
+        */
 
         override fun onScroll(
             e1 : MotionEvent, e2 : MotionEvent,
@@ -188,6 +255,12 @@ class GameActivity : AppCompatActivity()
         ) : Boolean
         {
             showToast("onScroll")
+
+            if(currentInstruction == possibleIntructions[1])
+            {
+                correctPerformance()
+            }
+
             return true
         }
     }
