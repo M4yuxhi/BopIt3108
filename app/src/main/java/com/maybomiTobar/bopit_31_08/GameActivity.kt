910b514411b04b58bdd45b3149f1cfa5cf9b8d00
M4yuxhi <91566436+M4yuxhi@users.noreply.github.com>
2023-11-13 15:14:25 -0300

    Complete Game Part 1

--- app/src/main/java/com/maybomiTobar/bopit_31_08/GameActivity.kt
+++ app/src/main/java/com/maybomiTobar/bopit_31_08/GameActivity.kt
@@ -1,6 +1,8 @@
 package com.maybomiTobar.bopit_31_08
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.Resources
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -11,13 +13,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
+import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.preference.PreferenceManager
 import kotlin.random.Random
-import kotlin.math.abs
+import kotlin.math.sqrt
 
 class GameActivity : AppCompatActivity(), SensorEventListener
 {
@@ -31,19 +35,24 @@ class GameActivity : AppCompatActivity(), SensorEventListener
 
     private lateinit var sensorManager : SensorManager
     private var accelerometerSensor : Sensor? = null
-    private var prevValueAcceleration = 0.0f
     private var currentInstruction = " "
     private lateinit var instructionTV: TextView
     private lateinit var instructionTimeTV : TextView
-    private lateinit var possibleIntructions : Array<String>
+    private lateinit var possibleInstructions : Array<String>
     private var instructionTimer : CountDownTimer? = null
-    private val instructionTime = 5000
+    private var instructionTime = 5000
 
     private lateinit var scoreTV : TextView
     private var score = 0
     private var asignedScore = 100
 
     private var actionWasPerformed = false
+    private var actionsWellPerformed = 0
+    private var prevRandomNum = -1
+
+    private var thresholdDifficultyIncrease = 8
+    private var highscore = 0
+    private lateinit var sharePref : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -58,7 +67,16 @@ class GameActivity : AppCompatActivity(), SensorEventListener
         instructionTV = findViewById(R.id.instructionTVG)
         instructionTimeTV = findViewById(R.id.instructionTimeTVG)
         scoreTV = findViewById(R.id.scoreTVG2)
-        possibleIntructions = arrayOf("Toca Bop It", "Desliza", "Agita")
+
+        sharePref = PreferenceManager.getDefaultSharedPreferences(this)
+        val tDIPref = sharePref.getString("thresholdDifficultyIncreasePreference", "8")
+        thresholdDifficultyIncrease = Integer.parseInt(tDIPref)
+
+        possibleInstructions = arrayOf(
+            applicationContext.getString(R.string.BotIpPressInstruct),
+            applicationContext.getString(R.string.SwipeInstruct),
+            applicationContext.getString(R.string.ShakeInstruct)
+        )
 
         initializeBGMediaPlayer()
 
@@ -68,11 +86,12 @@ class GameActivity : AppCompatActivity(), SensorEventListener
 
     override fun onPause()
     {
-        super.onPause()
         backgroundMediaPlayer.pause()
-        fxMediaPlayer.stop()
+        fxMediaPlayer.pause()
 
         sensorManager.unregisterListener(this)
+        instructionTimer?.cancel()
+        super.onPause()
     }
 
     override fun onResume()
@@ -96,17 +115,15 @@ class GameActivity : AppCompatActivity(), SensorEventListener
             val y = event.values[1]
             val z = event.values[2]
 
-            val diff = abs(x + y + z - prevValueAcceleration)
-            val scaleFactor = Math.pow(10.0, 10.0).toFloat()
-            val acceleration = (diff / event.timestamp) * scaleFactor
+            val acceleration = sqrt(x * x + y * y + z * z.toDouble()).toFloat()
 
-            prevValueAcceleration = x + y + z
+            val threshold = 25f
 
-            val threshold = 0.01f
-
-            if(acceleration > threshold && currentInstruction == possibleIntructions[2])
+            if(acceleration > threshold && currentInstruction == possibleInstructions[2])
+            {
+                Log.i("ValidationOnSensorChange", acceleration.toString() + " > " + threshold.toString())
                 correctPerformance()
-
+            }
         }
     }
 
@@ -121,10 +138,17 @@ class GameActivity : AppCompatActivity(), SensorEventListener
     {
         scoreTV.text = score.toString()
 
-        val random = Random
-        val randomNumber = random.nextInt(1, 4)
+        if (actionsWellPerformed >= 8 && instructionTime > 1)
+        {
+            instructionTime -= 250
+            actionsWellPerformed = 0
+        }
 
-        currentInstruction = possibleIntructions[randomNumber - 1]
+        var randomNumber = getRandomInstruction()
+        prevRandomNum = randomNumber
+
+
+        currentInstruction = possibleInstructions[randomNumber - 1]
         instructionTV.text = currentInstruction
 
         instructionTimer?.cancel()
@@ -141,7 +165,9 @@ class GameActivity : AppCompatActivity(), SensorEventListener
             {
                 if(!actionWasPerformed)
                 {
-                    setMusicOnFXMP(R.raw.lose_sound)
+                    setMusicOnFXMP(R.raw.lose_sound, false)
+                    checkHighscore()
+                    finish()
                 }
 
                 fxMediaPlayer.start()
@@ -154,19 +180,44 @@ class GameActivity : AppCompatActivity(), SensorEventListener
         actionWasPerformed = false
     }
 
-    private fun changeSoundSpeed(scalar : Float)
+    private fun changeSoundSpeedByScalar(scalar : Float)
     {
         backgroundMediaPlayer.playbackParams.setSpeed(playbackParams.speed * scalar)
+    }
+
+    private fun checkHighscore()
+    {
+        sharePref = PreferenceManager.getDefaultSharedPreferences(this)
+        val highscorePref = sharePref.getString("highscorePreference", "0")
+        highscore = Integer.parseInt(highscorePref)
+
+        if(score > highscore)
+        {
+            sharePref.edit().putString("highscorePreference", score.toString()).apply()
+        }
     }
 
     private fun correctPerformance()
     {
         //showToast(R.string.correctActionMessage.toString())
         actionWasPerformed = true
+        actionsWellPerformed++
         score += asignedScore
-        setMusicOnFXMP(R.raw.win_sound)
+        setMusicOnFXMP(R.raw.win_sound, false)
         bopItLogic()
     }
+
+    private fun getRandomInstruction() : Int
+    {
+        //var randomValue : Int
+        val random = Random
+        return random.nextInt(1, 4)
+        /*
+        do{ randomValue = random.nextInt(1, 4) }while(randomValue == prevRandomNum)
+        return randomValue
+        */
+    }
+
 
     private fun handlers()
     {
@@ -190,9 +241,10 @@ class GameActivity : AppCompatActivity(), SensorEventListener
         backgroundMediaPlayer.playbackParams.setSpeed(1.0f)
     }
 
-    private fun setMusicOnFXMP(id : Int)
+    private fun setMusicOnFXMP(id : Int, loop : Boolean)
     {
         fxMediaPlayer = MediaPlayer.create(this, id)
+        fxMediaPlayer.isLooping = loop
         fxMediaPlayer.start()
     }
 
@@ -202,7 +254,7 @@ class GameActivity : AppCompatActivity(), SensorEventListener
 
         buttonBopIt.setOnClickListener()
         {
-            if(currentInstruction == possibleIntructions[0])
+            if(currentInstruction == possibleInstructions[0])
             {
                 correctPerformance()
             }
@@ -254,9 +306,9 @@ class GameActivity : AppCompatActivity(), SensorEventListener
             distanceX : Float, distanceY : Float
         ) : Boolean
         {
-            showToast("onScroll")
+            //showToast("onScroll")
 
-            if(currentInstruction == possibleIntructions[1])
+            if(currentInstruction == possibleInstructions[1])
             {
                 correctPerformance()
             }
